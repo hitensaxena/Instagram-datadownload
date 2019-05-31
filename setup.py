5dfd7491fa5887e7ea383fec6aed392c0afe20f6
Hiten Saxena <developerhiten@gmail.com>
2019-06-01 00:17:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ if sys.version_info < (3, 2):
     requires.append('configparser')
 
 setup(
-    name='instagram-scraper',
+    name='instagram-datadownloader',
     version='1.6.1',
     description=("instagram-scraper is a command-line application written in Python"
                  " that scrapes and downloads an instagram user\'s photos and videos. Use responsibly."),
-    url='https://github.com/rarcega/instagram-scraper',
-    download_url='https://github.com/rarcega/instagram-scraper/tarball/1.6.1',
+    url='https://github.com/narrativewolf/Instagram-datadownload',
+    download_url='https://github.com/narrativewolf/Instagram-datadownload/tarball/1.6.1',
     author='Richard Arcega',
     author_email='hello@richardarcega.com',
     license='Public domain',
